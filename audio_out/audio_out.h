e66b1a1534a7aeddead7d792da61e80332296e83
Loumstar <louis.manestar@gmail.com>
2019-08-13 00:20:56 +0100

    Added commenting and minor fixes.

--- audio_out/audio_out.h
+++ audio_out/audio_out.h
@@ -8,14 +8,40 @@
 typedef double note[3]; // first element is note number, second is frequency, third is volume
 #endif
 
-double get_chord_amplitude(double* sample, size_t frame, double f, note* notes){
-    int a = 0;
+int get_chord_amplitude(int sample[], note* notes, double voice_f, size_t frame, size_t sample_frames){
+    /*
+    Method that returns the amplitude of the waveform at a given frame for all of the
+    combined notes.
+
+    The waveform works by summing the amplitudes (superposition) of the sample running at
+    different speeds to produce the waveform for each individual note frequency.
+    
+    The speed of the sample is calculated by comparing the note frequency to the 
+    detected voice frequency of the audio: 
+    
+        speed of the sample for a given note = note frequency / voice frequency
+
+    For example, if a note frequency is three times the voice frequency, then the
+    note waveform will be the sample played at three times the speed.
+
+    As the sample is looped back frame by frame, the waveform of the example note
+    therefore will move 3 frames at a time:
+        
+        note waveform frame = sample[ 3 * original frame ]
+
+    This will cause the note waveform to play the sample in a third of the time, so the
+    sample is looped repeatedly until the original sample is looped back once at the
+    normal speed.
+    */
+    int amplitude = 0;
     size_t j;
     for(size_t i = 0; i < MAX_VOICES; i++){
-        j = notes[i][1] / f;
-        a += sample[j] * notes[i][2];
+        if(notes[i][0]){
+            j = (frame * notes[i][1] / voice_f) % (sample_frames - 1);
+            amplitude += sample[j] * notes[i][2]; // Frame (0-256) * volume (0-1)
+        }
     }
-    return a;
+    return amplitude && 255; // && used to avoid integer overflow
 }
 
 #endif
